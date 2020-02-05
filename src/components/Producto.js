79830da5d632e0c4337625035ec74c37c6f74049
Checho <checho9999@gmail.com>
2020-02-05 19:37:20 -0300

    Nuevo componente Carrito que reutiliza el componente Producto; Tambien se agregaron las opciones de agregar y eliminar productos del carrito de compras

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -7,21 +7,44 @@ const { id, nombre, precio } = producto;
 
     // Agregar producto al carrito
     const seleccionarProducto = id => {
-        const producto = productos.filter(producto => producto.id === id )[0];        
+        const producto = productos.filter(producto => producto.id === id )[0];
+        
         agregarProducto([
             ...carrito, 
             producto
         ]);
+        
+    }
+
+    // Elimina un producto del carrito
+    const eliminarProducto = id => {
+        const productos = carrito.filter(producto => producto.id !== id);
+
+        // Colocar los productos en el state
+        agregarProducto(productos)
     }
 
     return ( 
         <div>
-           <h2>{nombre}</h2>
-           <button 
-                type="button"
-                onClick={ () => seleccionarProducto(id) }
-           >Comprar</button>
-           <p>${precio}</p>
+            <h2>{nombre}</h2>
+            <p>${precio}</p>
+            
+            { productos 
+            ?
+                (
+                    <button 
+                        type="button"
+                        onClick={ () => seleccionarProducto(id) }
+                    >Comprar</button>
+                )
+            : 
+                (
+                    <button 
+                        type="button"
+                        onClick={ () => eliminarProducto(id) }
+                    >Eliminar</button>
+                )
+            }
         </div>             
      );
 }
